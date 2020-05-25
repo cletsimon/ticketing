88e79ea4a0dbc7f4bb87ac004160952342ac7d1e
Clet Simon Sevillejo <cssevillejo@gmail.com>
2020-05-25 23:28:45 +0800

    move to current-user logic to a middlware

---
+++ auth/src/middlewares/current-user.ts
@@ -0,0 +1,39 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+interface UserPayload {
+  id: string;
+  email: string;
+}
+
+// this is how we can reach to an existing type definition
+// and make a modification to it
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: UserPayload;
+    }
+  }
+}
+
+export const currentUser = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!req.session || !req.session.jwt) {
+    // does not work
+    //if (!req.session?.jwt) {
+    return next();
+  }
+
+  try {
+    const payload = jwt.verify(
+      req.session.jwt,
+      process.env.JWT_KEY!
+    ) as UserPayload;
+    req.currentUser = payload;
+  } catch (err) {}
+
+  next();
+};
