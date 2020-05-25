559263a6dd9657a98703e4146cd76f96afdff4e4
Clet Simon Sevillejo <cssevillejo@gmail.com>
2020-05-25 17:53:04 +0800

    formatting JSON properties

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -21,16 +21,30 @@ interface UserDoc extends mongoose.Document {
   password: string;
 }
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    }
   },
-  password: {
-    type: String,
-    required: true
+  {
+    // this is not the best approach mvc
+    // the transformation should be on view
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      }
+    }
   }
-});
+);
 
 userSchema.pre('save', async function(done) {
   if (this.isModified('password')) {
