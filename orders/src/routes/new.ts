c236603d68ba6e112b98355c48b275aaed019c38
Clet Simon Sevillejo <cssevillejo@gmail.com>
2020-06-09 00:04:46 +0800

    convenience document methods

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,7 +1,15 @@
 import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
-import { requireAuth, validateRequest } from '@tiktix/common';
+import {
+  requireAuth,
+  validateRequest,
+  NotFoundError,
+  OrderStatus,
+  BadRequestError,
+} from '@tiktix/common';
 import { body } from 'express-validator';
+import { Ticket } from '../models/ticket';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
@@ -17,6 +25,26 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+
+    // Find the ticket the user is trying to order in the database
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    // Make sure that this ticket is not already reserved
+    const isReserved = await ticket.isReserved();
+    if (isReserved) {
+      throw new BadRequestError('Ticket is already reserved');
+    }
+
+    // Calculate an expirate date for this order
+
+    // Build the order and save it to the database
+
+    // Publish an event saying that an order was created
+
     res.send({});
   }
 );
