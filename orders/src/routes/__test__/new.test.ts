75d7b226547e97f2472c63c09ebfd21890eab566
Clet Simon Sevillejo <cssevillejo@gmail.com>
2020-06-09 22:10:50 +0800

    complete new orders test

---
+++ orders/src/routes/__test__/new.test.ts
@@ -0,0 +1,62 @@
+import mongoose from 'mongoose';
+import request from 'supertest';
+import { app } from '../../app';
+import { Order, OrderStatus } from '../../models/order';
+import { Ticket } from '../../models/ticket';
+
+/**
+ * TODO
+ * test if the user is authenticated
+ * test if valid ticket ID
+ */
+it('returns an error if the ticket does not exist', async () => {
+  const ticketId = mongoose.Types.ObjectId();
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({
+      ticketId,
+    })
+    .expect(404);
+});
+
+it('returns an error if the ticket is already reserved', async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+
+  const order = Order.build({
+    ticket,
+    userId: 'lsdjflsdflsdf',
+    status: OrderStatus.Created,
+    expiresAt: new Date(),
+  });
+  await order.save();
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({
+      ticketId: ticket.id,
+    })
+    .expect(400);
+});
+
+it('reserves a ticket', async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({
+      ticketId: ticket.id,
+    })
+    .expect(201);
+});
