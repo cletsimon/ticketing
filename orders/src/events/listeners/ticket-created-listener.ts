237138f79217c95bdfa21394649b417315c3e4f5
Clet Simon Sevillejo <cssevillejo@gmail.com>
2020-06-12 09:46:45 +0800

    blueprint listeners

---
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -0,0 +1,11 @@
+import { Message } from 'node-nats-streaming';
+import { Subjects, Listener, TicketCreatedEvent } from '@tiktix/common';
+import { Ticket } from '../../models/ticket';
+import { queueGroupName } from './queue-group-name';
+
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  readonly subject = Subjects.TicketCreated;
+  queueGroupName = queueGroupName;
+
+  onMessage(data: TicketCreatedEvent['data'], msg: Message) {}
+}
