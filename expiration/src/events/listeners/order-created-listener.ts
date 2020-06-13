65e5154c1dde59dc408425384e54a961151cd936
Clet Simon Sevillejo <cssevillejo@gmail.com>
2020-06-13 12:52:09 +0800

    queuing a job event arrival

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -1,10 +1,17 @@
-import { Message } from 'node-nats-streaming';
 import { Listener, OrderCreatedEvent, Subjects } from '@tiktix/common';
+import { Message } from 'node-nats-streaming';
 import { queueGroupName } from './queue-group-name';
+import { expirationQueue } from '../../queues/expiration-queue';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
 
-  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {}
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    await expirationQueue.add({
+      orderId: data.id,
+    });
+
+    msg.ack();
+  }
 }
