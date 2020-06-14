baf472ad144399f7f7430aa115700b5bc8b84b34
Clet Simon Sevillejo <cssevillejo@gmail.com>
2020-06-14 19:59:01 +0800

    test failing test

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -17,7 +17,7 @@ it('responds with null if not authenticated', async () => {
   const response = await request(app)
     .get('/api/users/currentuser')
     .send()
-    .expect(200);
+    .expect(400);
 
   expect(response.body.currentUser).toEqual(null);
 });
