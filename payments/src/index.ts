ee5722f46a68f2b8a5b520ef89ef8b8d49951332
Clet Simon Sevillejo <cssevillejo@gmail.com>
2020-06-14 22:35:43 +0800

    test deployment

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -5,6 +5,7 @@ import { OrderCreatedListener } from './events/listeners/order-created-listener'
 import { OrderCancelledListener } from './events/listeners/order-cancelled-listener';
 
 const start = async () => {
+  console.log('Starting ....');
   // check if env key is set
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
